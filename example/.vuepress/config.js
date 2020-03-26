36f97be9baaa85f6a4317559bbf9da86438087c8
xdhan <dongdohan@qq.com>
2020-03-26 23:46:26 +0800

    start

---
+++ example/.vuepress/config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+    theme: path.resolve(__dirname, '../../'),
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    markdown: {
+        lineNumbers: true
+    },
+    // permalink: "/:year/:month/:day/:slug",
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'post', link: '/post/' },
+            { text: 'qq', link: '/post/qq' },
+        ]
+    }
+}
---
+++ layouts/Layout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <Home></Home>
+  </div>
+</template>
+
+<script>
+import Home from '@theme/components/Home.vue'
+import Navbar from '@theme/components/Navbar.vue'
+export default {
+  components: { Home, Navbar },
+}
+</script>
