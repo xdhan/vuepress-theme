ee190cdfbe40516278490bde6d6487d91cacb173
xdhan <dongdohan@qq.com>
2020-03-27 00:17:04 +0800

    first

--- components/Home.vue
+++ components/Home.vue
@@ -1,23 +1,14 @@
 <template>
   <div>
-    <NavLink class="action-button" :item="actionLink" />
     <Content class="theme-default-content" />
   </div>
 </template>
 
 <script>
-import NavLink from '@theme/components/NavLink.vue'
 export default {
-  components: { NavLink },
   computed: {
     data() {
       return this.$page.frontmatter
-    },
-    actionLink() {
-      return {
-        link: this.data.actionLink,
-        text: this.data.actionText,
-      }
     },
   },
 }
